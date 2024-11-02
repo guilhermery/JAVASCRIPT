b4ae17e30c40915f992577c7a39427628fb0f38f
guilhermery <guilhermeryan277@gmail.com>
2024-11-02 17:59:24 -0300

    adicionei aula 011 - condições

---
+++ aula 011/ex014.js
@@ -0,0 +1,6 @@
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+} /*Condição simples*/
+console.log(`Dirija sempre com cinto de segurança`);

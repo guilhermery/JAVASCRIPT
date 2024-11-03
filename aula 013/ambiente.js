741f62eec0fc09648d980cc23bfb30e87c23a582
guilhermery <guilhermeryan277@gmail.com>
2024-11-03 17:54:45 -0300

    Adicionei aula 013 - while e do while

---
+++ aula 013/ambiente.js
@@ -0,0 +1,16 @@
+/*console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');*/
+var i = 1;
+var c = 1;
+while (i <= 6) {
+    console.log(`Passo ${i}`);
+    i++
+}
+
+do {
+    console.log(`Passo ${c}`)
+} while(c <= 6)

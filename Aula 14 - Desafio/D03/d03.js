82ce49c32ddfe511ce19b3ba246cc587f576ad54
guilhermery <guilhermeryan277@gmail.com>
2024-11-04 18:57:55 -0300

    Modificação no erro

--- Aula 14 - Desafio/D03/d03.js
+++ Aula 14 - Desafio/D03/d03.js
@@ -3,12 +3,16 @@ function contar(){
     var i = Number(window.document.querySelector(`input#inicio`).value);
     var f = Number(window.document.querySelector(`input#fim`).value);
     var p = Number(window.document.querySelector(`input#passo`).value);
+    var emoji_mao = '\u{1F449}' /*Usar codigo do unicode*/
+    var emoji_chegada = '\u{1F3C1}'
+    res.innerHTML = "<p>Contando:</p>"
     if (p == 0){
-        window.alert(`[ERRO] O passo deve ser diferente de 0!`)
+        window.alert(`[ERRO] Passo invalido, considerando PASSO 1!`);
+        for(var c = i; c < f; c++){
+            res.innerHTML += `${c} ${emoji_mao} `
+        }
+        res.innerHTML += `${emoji_chegada}`
     }else{
-        var emoji_mao = '\u{1F449}' /*Usar codigo do unicode*/
-        var emoji_chegada = '\u{1F3C1}'
-        res.innerHTML = "<p>Contando:</p>"
         for(var c = i; c < f; c = c + p){
             res.innerHTML += `${c} ${emoji_mao} `
         }

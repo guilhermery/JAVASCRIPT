77c728d53055b9de4f33d3f271c50cb8eed06fc3
guilhermery <guilhermeryan277@gmail.com>
2024-11-05 14:53:03 -0300

    Adicionei aviso de erro e lógica da tabuada

--- Aula 14 - Desafio/D04/d04.js
+++ Aula 14 - Desafio/D04/d04.js
@@ -3,7 +3,11 @@ function tabuada(){
     var n = window.document.querySelector(`input#numero`)
     if(n.value.length == 0){
         window.alert(`[ERRO!] Digite um número`)
-    }
-    for(c = 0;c <= 10; c++){
+    } else{
+        var n = Number(n.value)
+        for(var c = 0;c <= 10; c++){
+            var r = n*c;
+            res.innerHTML += `<p>${n} X ${c} = ${r}</p>`
+        }
     }
 }

4432e111f71d75892f676e9f9696033470fe1bed
guilhermery <guilhermeryan277@gmail.com>
2024-11-08 16:33:05 -0300

    Finalizei a função da calculadora

--- Calculadora IMC/script/script.js
+++ Calculadora IMC/script/script.js
@@ -1,8 +1,14 @@
 function calcula(){
-    let peso = parseFloat(window.document.querySelector(`input#peso`))
-    let altura = parseFloat(window.document.querySelector(`input#altura`))
+    let peso = window.document.querySelector(`input#peso`).value.replace(',', '.')
+    let altura = window.document.querySelector(`input#altura`).value.replace(',', '.') /**/
     let text = window.document.querySelector(`span#imc`)
-    altura = altura*altura;
-    let imc = (peso/altura).toFixed(2)
-    
+    if(Number(altura) <= 0 || Number(peso) <= 0 || altura === '' || peso === '') {
+        window.alert(`[ERRO!] Verifique se inseriu valores válidos`)
+    } else {
+        peso = Number(peso)
+        altura = Number(altura)
+        let imc = peso / (altura*altura)
+        text.innerHTML = ``
+        text.innerHTML = `Seu IMC: ${imc.toFixed(2)}`
+    }
 }

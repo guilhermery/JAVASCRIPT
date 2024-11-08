df5ad8074285b1921df0f1d2b5aa5aaf7d2f6ad5
guilhermery <guilhermeryan277@gmail.com>
2024-11-07 23:41:33 -0300

    Adicionei comentários explicativos em cada parte

--- Desafio 06/script06.js
+++ Desafio 06/script06.js
@@ -15,8 +15,9 @@ const relogio = setInterval(function time(){
     }
     if (s < 10){
         s = '0' + s
-    }
-    horas.innerHTML = hr
-    minutos.innerHTML = mins
-    segundos.innerHTML = s
-}) /*Essa função funciona em um loop infinito enquanto o site estiver aberto ou receber um clearInterval()*/
+    } /*Para adicionar um 0 antes do horario, ex: 09 horas 12 minutos e 03 segundos*/
+    horas.textContent = hr
+    minutos.textContent = mins
+    segundos.textContent = s /*Se usa textContent, pois ele sempre trata tudo como string puro, sem formatação e sem considerar números. O innerHTML trata números como números, logo ele não vê sentido adicionar um 0 a esquerda do número e acaba descartando. Desssa forma, se quiser usar innerHTML deve transferir a variavel para string com innerHTML = String(variavel)*/
+}) 
+/*Essa função funciona em um loop infinito enquanto o site estiver aberto ou receber um clearInterval()*/

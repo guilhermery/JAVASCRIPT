076cda284cfc9a0c5fe5b53e7d5a2e35dbe26427
guilhermery <guilhermeryan277@gmail.com>
2024-11-08 16:35:06 -0300

    Adicionei algumas explicações

--- Calculadora IMC/script/script.js
+++ Calculadora IMC/script/script.js
@@ -1,6 +1,6 @@
 function calcula(){
     let peso = window.document.querySelector(`input#peso`).value.replace(',', '.')
-    let altura = window.document.querySelector(`input#altura`).value.replace(',', '.') /**/
+    let altura = window.document.querySelector(`input#altura`).value.replace(',', '.') /*Ele faz a conversão de todas as virgulas para um ponto, para poder se encaixar como número.*/
     let text = window.document.querySelector(`span#imc`)
     if(Number(altura) <= 0 || Number(peso) <= 0 || altura === '' || peso === '') {
         window.alert(`[ERRO!] Verifique se inseriu valores válidos`)

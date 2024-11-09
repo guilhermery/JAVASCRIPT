05d09717f8ef37e2da812a09de65749906a1d1cc
guilhermery <guilhermeryan277@gmail.com>
2024-11-09 17:30:51 -0300

    update

--- aula 018 - funções de string/ambiente.js
+++ aula 018 - funções de string/ambiente.js
@@ -1,10 +1,10 @@
 // length
 
-const nome = "Matheus"
+let nome = "Matheus"
 
 console.log(nome.length)
 
-const obj = "bola"
+let obj = "bola"
 
 console.log(obj.length)
 
@@ -13,18 +13,21 @@ console.log(obj.length)
 console.log(nome[0])
 console.log(nome[2])
 
-const frase = "O rato roeu a roupa do rei de roma"
+let frase = "O rato roeu a roupa do rei de roma"
 
 console.log(frase.indexOf('roeu')) //Em que numero começa a frase roeu
 
 //slice: pega uma parte especifica da string
 
-const roeu = frase.slice(7, 11) //Ele recorta de um número ate outro (recortei a partir do r de roeu ate o fim e adicionei um para o /0)
+let roeu = frase.slice(7, 11) //Ele recorta de um número ate outro (recortei a partir do r de roeu ate o fim e adicionei um para o /0)
 
 console.log(roeu)
 
 //replace: serve para trocar frases ou espaços por outros
 
-const novafrase = frase.replace("roeu", "teste")
+let novafrase = frase.replace("roeu", "teste")
 
 console.log(novafrase)
+
+//toLowerCase e toUpperCase: caixa alta e caixa baixa
+

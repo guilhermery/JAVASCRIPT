226ebf53da5267200320877bebf4970e7ad8b216
guilhermery <guilhermeryan277@gmail.com>
2024-11-10 14:24:04 -0300

    add aula 019

---
+++ aula 019 - arrays/aula.js
@@ -0,0 +1,17 @@
+let arr = [1, 'Guilherme', true, {teste: 1, teste: 2}] //arrays são variaveis que podem armazenar mais de um tipo de variavel
+
+console.log(arr)
+
+let arr2 = [1, 4, 5, 2, 3] //arrays podem ser de um mesmo tipo apenas
+
+console.log(arr2)
+
+console.log(arr[0]) //acessar primeiro elemento
+console.log(arr[1]) //acessar segundo elemento
+
+arr2[5] = 6 //podemos adicionar um elemento no array dessa forma
+
+arr[0] = 'Teste' //podemos modificar um elemento do array dessa forma
+
+console.log(arr2)
+console.log(arr)

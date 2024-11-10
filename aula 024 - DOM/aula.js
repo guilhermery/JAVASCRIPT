21cc0017475d5b1957378281e84153d7f63de27f
guilhermery <guilhermeryan277@gmail.com>
2024-11-10 17:35:27 -0300

    adicionei aula 24 - DOM

---
+++ aula 024 - DOM/aula.js
@@ -0,0 +1,9 @@
+//O DOM é conhecido como Document Object Model, e torna possível acessar, criar ou deletar elementos do HTML, acessando propriedades e metodos
+
+//É uma interface de programação para HTML, podendo alterar o HTML e estilo, fazer animações, elevar elementos ou criar menus, tudo podendo ser feito através do DOM por meio do JavaScript
+
+//Logo, o DOM é uma espécie de cópia do HTML, tendo metodos para acessar ele e alterá-lo, consequentemente, alterando o HTML
+
+//Podemos manipular eventos pelo DOM que afetam o HTML
+
+//Sempre é necessário investigar a árvore DOM para acessar os elementos 

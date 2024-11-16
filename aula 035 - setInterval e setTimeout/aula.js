00cadf324a9544d286794734658ad5feabd65fdd
guilhermery <guilhermeryan277@gmail.com>
2024-11-16 14:52:22 -0300

    Adicionei aula 035 sobre setTimeout e setInterval

--- aula 035 - setInterval e setTimeout/aula.js
+++ aula 035 - setInterval e setTimeout/aula.js
@@ -3,3 +3,16 @@
 
 //setTimeout: executa uma função após um tempo
 
+console.log(`Antes do setTimeout`)
+
+setTimeout(function() {
+    console.log(`Testando o setTimeout`)
+}, 1) //Os parametros são uma função de callback e o tempo que a função será executada em milissegundos
+
+console.log(`Depois do setTimeout`) //Mesmo estando após a função, como a função timeout é assincrona, ela não segue a ordem sincrona e não interfere na ordem dos elementos, ela apenas executa após o tempo determinado
+
+//setInterval: executa uma função após um tempo e continua executndo ela após cada intervalo de tempo definido
+
+setInterval(function(){
+    console.log(`Testando o setInterval`)
+}, 1000) //Também tem os mesmos parametros do setTimeout

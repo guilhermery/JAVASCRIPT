5bb3d850734693e00fceeee009c213b9fd1c361d
guilhermery <guilhermeryan277@gmail.com>
2024-11-16 14:25:11 -0300

    fiz algumas anotações na aula 034

--- aula 034 - Callback functions/aula.js
+++ aula 034 - Callback functions/aula.js
@@ -4,18 +4,18 @@
 
 function exibir(num){
     console.log(`A operação resultou em ${num}`)
-}
+} //Essa será a função de callback que precisa ser chamada
 
 function soma(a, b, callback){
     let op = a + b;
     callback(op)
-}
+} //Essa é a função soma que usa a função callback para exibir o resultado da operação
 
 function multiplicacao(a, b, cb){
     let op = a * b;
     cb(op);
-}
+} //Essa éa  função multiplicação que usa a função callback para exibir o resultado da operação
 
-soma(2, 2, exibir)
+soma(2, 2, exibir) //Aqui botamos os parametros e o callback
 
 multiplicacao(2, 4, exibir)

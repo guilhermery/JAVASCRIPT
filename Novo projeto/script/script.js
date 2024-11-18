15a46f3d1c27bf48cfb31e105ead06696b76d1e8
guilhermery <guilhermeryan277@gmail.com>
2024-11-18 20:52:08 -0300

    Adicionei o forms, css de algumas partes e o script

---
+++ Novo projeto/script/script.js
@@ -0,0 +1,9 @@
+function cor(){
+    let botao = window.document.querySelector(`button.ToDo`)
+    botao.style.cssText =`background-color: rgb(54, 0, 54); transition-duration: 0.5s; color: white;`
+}
+
+function volta(){
+    let botao = window.document.querySelector(`button.ToDo`)
+    botao.style.cssText =`background-color: white; transition-duration: 0.5s; color: black;`
+}

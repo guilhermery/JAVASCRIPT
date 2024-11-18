40ab4ab780b06fbde3b7fcfa8ecc8afa20ba46b7
guilhermery <guilhermeryan277@gmail.com>
2024-11-18 11:40:15 -0300

    Adicionei o codigo da aula

--- aula 040 - keyup e keydown/aula.js
+++ aula 040 - keyup e keydown/aula.js
@@ -1,2 +1,18 @@
 //O evento keydown é ativado quando uma tecla é pressionada
 //Quando a tecla volta a posição normal, podemos atrelar um evento keyup
+
+//Keydown
+
+document.addEventListener(`keydown`, function kd(event){
+    console.log(event.key)
+    if(event.key == `Enter`){
+        console.log("Você pressionou enter!")
+    }
+})
+
+//Keyup
+document.addEventListener(`keyup`, function ku(event){
+    if(event.key == `Enter`){
+        console.log(`Soltou o enter`)
+    }
+})

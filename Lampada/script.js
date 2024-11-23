a023b4f4802fab6c87ac3c67db69e68a41ce2504
guilhermery <guilhermeryan277@gmail.com>
2024-11-23 14:53:04 -0300

    adicionei função de mouseout

--- Lampada/script.js
+++ Lampada/script.js
@@ -1,4 +1,9 @@
 const imagem = window.document.querySelector(`img#imagem`)
+
 imagem.addEventListener(`mouseover`, function liga(){
     imagem.setAttribute(`src`, `imagens/ligada.jpg`)
+})
+
+imagem.addEventListener(`mouseout`, function desliga(){
+    imagem.setAttribute(`src`, `imagens/desligada.jpg`)
 })

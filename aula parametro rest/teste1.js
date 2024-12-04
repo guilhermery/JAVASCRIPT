1f5d5c2a396dee29ff608280594c12b7bfc1b396
guilhermery <guilhermeryan277@gmail.com>
2024-12-03 21:18:54 -0300

    Adicionei anotações

--- aula parametro rest/teste1.js
+++ aula parametro rest/teste1.js
@@ -4,6 +4,7 @@ function soma(n1, n2){
 
 console.log(soma(10,5))
 
+//Utiliza-se o spread para transformar os valores passados como parametros em elementos de um array "valores".
 function somanovo(...valores){
     let tam = valores.length
     let res = 0
@@ -11,6 +12,6 @@ function somanovo(...valores){
         res = res + valores[i];
     }
     return res
-}
+} //Para essa função, primeiro foi necessario encontrar o tamanho do array valores (que indica a quantidade de elementos que está dentro dele) e apos isso usar o for para percorrer cada elemento e somar.
 
 console.log(somanovo(10, 5, 12, 15, 9))
--- aula parametro rest/teste2.js
+++ aula parametro rest/teste2.js
@@ -1,15 +1,17 @@
+//Podemos usar funções de parametro rest quando não sabemos quantos valores iremos utilizar de parametro
+
 function soma(n1, n2){
     return n1+n2;
-}
+} //Essa é uma função de soma comum com dois parametros
 
-console.log(soma(10,5))
+console.log(soma(10,5)) //Exibe o resultado da função soma
 
 function somanovo(...valores){
-    let res = 0
+    let res = 0 //inicializei a variavel res com 0
     for(let i of valores){
-        res = res + i;
+        res = res + i; //Para i percorrendo a variavel valores, somamos o resultado com ele mesmo + os valores de i.
     }
     return res
-}
+} //Essa é a função soma com o parametro rest e o for of que percorre todos os numeros presentes no array valores
 
-console.log(somanovo(10, 5, 12, 15, 9))
+console.log(somanovo(10, 5, 12, 15, 9)) //Exibe o resultado da função soma de parametro rest

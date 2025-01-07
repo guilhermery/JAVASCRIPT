1cc1c76e0969617ea6e6e722bdbcc79967213279
guilhermery <guilhermeryan277@gmail.com>
2025-01-07 14:51:31 -0300

    adicionei javascript

--- Aula query selector/aula 01.js
+++ Aula query selector/aula 01.js
@@ -3,13 +3,21 @@ const todosCursos = [...window.document.getElementsByClassName("curso")]
 const cursosC1 = [...window.document.getElementsByClassName("c1")]
 const cursosC2 = [...window.document.getElementsByClassName("c2")]
 const cursoEspecial = window.document.getElementsByClassName("curso")[6]
+const cursoEspecial2 = window.document.getElementById("c1")
 
-console.log(todasDiv)
+const query_umP = window.document.querySelector("div > p")
+const query_todasDiv = [...window.document.querySelectorAll("div")]
+const query_todosCurso = [...window.document.querySelectorAll(".curso")]
+
+console.log (query_umP)
+console.log(query_todosCurso)
+console.log(query_todasDiv)
+/*console.log(todasDiv)
 console.log(todosCursos)
 console.log(cursosC1)
 console.log(cursosC2)
-console.log(cursoEspecial)
+console.log(cursoEspecial)*/
 
-el.map((e,i)=>{
-    e.innerHTML="CFB Cursos"
+query_todosCurso.map((element,i)=>{
+    element.style.bordercolor ="Blue"
 })

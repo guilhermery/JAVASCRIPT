fd499dc850ed256a24aa61c69084aafbfd03d7de
guilhermery <guilhermeryan277@gmail.com>
2025-01-14 20:38:24 -0300

    Adicionei o antes, d

--- Aula metodoFilter/aula.js
+++ Aula metodoFilter/aula.js
@@ -3,8 +3,8 @@ const btn_c = [...window.document.querySelectorAll(`.curso`)]
 const c1_2 = window.document.querySelector(`#c1_2`)
 let cursos = ["HTML", "CSS", "JavaScript", "PHP", "React", "MySQL", "ReactNative"]
 const btnCursoSelecionado = window.document.getElementById(`btnCursoSelecionado`)
-const btnAddCursoAntes = window.document.getElementById(`btnAdicionarNovoCurso`)
-const btnAddCursoDepois = window.document.getElementById(`btnAdicionarDepois`)
+const btnAddAntes = window.document.getElementById(`btnAdicionarNovoCurso`)
+const btnAddDepois = window.document.getElementById(`btnAdicionarDepois`)
 const btnRemoverCurso = window.document.getElementById(`btnRemoverCurso`)
 const inputCurso = window.document.querySelector(`#nomeCurso`)
 
@@ -34,35 +34,91 @@ btnCursoSelecionado.addEventListener(`click`, ()=>{
         return ele.checked
     })
     radioSelecionado = radioSelecionado[0]
-    const cursoSelecionado = radioSelecionado.parentNode.previousSibling.textContent
-    alert(`curso selecionado: ${cursoSelecionado}`)
+    if(radioSelecionado){
+        const cursoSelecionado = radioSelecionado.parentNode.previousSibling.textContent
+        alert(`curso selecionado: ${cursoSelecionado}`)
+    } else {
+        alert(`Selecione um curso`)
+    }
 })
 
-btnAddCurso.addEventListener(`click`,()=>{
+btnAddAntes.addEventListener(`click`,()=>{
+    const todosRadios = [...window.document.querySelectorAll(`input[type=radio]`)]
+    let radioSelecionado = todosRadios.filter((ele, ind, arr)=>{
+        return ele.checked
+    })
+    radioSelecionado = radioSelecionado[0]
+    if(radioSelecionado){
+        if(inputCurso.value.trim() !== ""){
+            const cursoRef = radioSelecionado.parentNode.parentNode
+
+            const textoCurso = inputCurso.value.trim()
     
-    if(inputCurso.value.trim() !== ""){
-        const textoCurso = inputCurso.value.trim()
+            const novoCurso = window.document.createElement(`div`)
+            novoCurso.setAttribute(`class`, `curso c1`)
+            novoCurso.innerHTML = textoCurso
+    
+            const comandos = window.document.createElement(`div`)
+            comandos.setAttribute(`class`, `comandos`)
+    
+            const rb = window.document.createElement(`input`)
+            rb.setAttribute(`type`,`radio`)
+            rb.setAttribute(`name`,`rb_curso`)
+    
+            comandos.appendChild(rb)
+            
+            novoCurso.appendChild(comandos)
+    
+            caixaCursos.appendChild(novoCurso)
 
-        const novoCurso = window.document.createElement(`div`)
-        novoCurso.setAttribute(`class`, `curso c1`)
-        novoCurso.innerHTML = textoCurso
-
-        const comandos = window.document.createElement(`div`)
-        comandos.setAttribute(`class`, `comandos`)
-
-        const rb = window.document.createElement(`input`)
-        rb.setAttribute(`type`,`radio`)
-        rb.setAttribute(`name`,`rb_curso`)
-
-        comandos.appendChild(rb)
-        
-        novoCurso.appendChild(comandos)
-
-        caixaCursos.appendChild(novoCurso)
-
-        inputCurso.value = ""
+            caixaCursos.insertBefore(novoCurso, cursoRef)
+    
+            inputCurso.value = ""
+        } else {
+            alert(`Digite o nome do curso na caixa de entrada`)
+        }
     } else {
-        alert(`Digite o nome do curso na caixa de entrada`)
+        alert(`Selecione o curso de referência`)
+    }
+})
+
+btnAddDepois.addEventListener(`click`, ()=>{
+    const todosRadios = [...window.document.querySelectorAll(`input[type=radio]`)]
+    let radioSelecionado = todosRadios.filter((ele, ind, arr)=>{
+        return ele.checked
+    })
+    radioSelecionado = radioSelecionado[0]
+    if(radioSelecionado){
+        if(inputCurso.value.trim() != ""){
+            const cursoRef = radioSelecionado.parentNode.parentNode.nextSibling
+
+            const textoCurso = inputCurso.value.trim()
+
+            const novoCurso = window.document.createElement(`div`)
+            novoCurso.setAttribute(`class`, `curso c1`)
+            novoCurso.innerHTML = textoCurso
+
+            const comandos = window.document.createElement(`div`)
+            comandos.setAttribute(`class`, `comandos`)
+
+            const rb = window.document.createElement(`input`)
+            rb.setAttribute(`type`,`radio`)
+            rb.setAttribute(`name`,`rb_curso`)
+
+            comandos.appendChild(rb)
+            
+            novoCurso.appendChild(comandos)
+
+            caixaCursos.appendChild(novoCurso)
+
+            caixaCursos.insertBefore(novoCurso, cursoRef)
+
+            inputCurso.value = ""
+        } else {
+            alert(`Digite o nome do curso na caixa de entrada`)
+        }
+    } else {
+        alert(`Selecione o curso de referência`)
     }
 })
 

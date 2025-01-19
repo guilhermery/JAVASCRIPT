1dc65c6b4dc9c74bbc901c084fb11b8580cf46c5
guilhermery <guilhermeryan277@gmail.com>
2025-01-19 17:10:11 -0300

    adicionei aula metodoSome

--- Aula metodoSome/aula.js
+++ Aula metodoSome/aula.js
@@ -5,12 +5,15 @@ const pResult = window.document.querySelector(`#resultado`)
 const array = [21, 25, 19, 20, 16, 18, 22]
 pArray.innerHTML = `[` + array + `]`
 
-btnVerificar.addEventListener(`click`,()=>{
+btnVerificar.addEventListener(`click`,(evt)=>{
     const ret = array.some((e,i)=>{
+        if(e<18){
+            pResult.innerHTML = `O array não conforme está na posição ${i+1}`
+        }
         return e>=18
     })
     if(ret){
-        pResult.innerHTML = `O elemento em conformidade ${e} está na posição ${i}`
+        pResult.innerHTML = `Há pelo menos um resultado equivalente a pesquisa`
     }
 })
 
